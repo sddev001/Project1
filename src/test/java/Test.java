7eeb412b4b0919fe9b1aea9fdb7eafe1ecfaceb1
Shibu <shibudev@gmail.com>
2015-05-30 01:13:46 -0400

    add sum

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,6 +3,12 @@
  */
 public class Test {
     public static void main(String[] args) {
+
         System.out.println("Hello");
+
+        int x = 5;
+        int y = 9;
+
+        System.out.println(x+y);
     }
 }
